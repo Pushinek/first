c28d0525466d8edb4ee35865755c942447de00a6
Pushinechek <Sanechka.pushin@gmail.com>
2024-01-18 17:37:34 +0400

    ктото я тебя люблю много раз с пояснениями

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-chislo = int(input())
+chislo = int(input('Число раз'))
 print(chisl)
-imya = input()
+imya = input('Имя')
 def lublu(name):
     print(f'{name}, я тебя люблю')
 for i in range(chislo):
